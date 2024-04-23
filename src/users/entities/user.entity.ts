6436bcd7507056ba212ae9b32dfd15b6021c6002
Olatunji Yusuf <olatunjiyso@gmail.com>
2024-04-23 21:42:13 +0100

    define entities

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop()
+  id: number;
+
+  @Prop()
+  firstname: string;
+
+  @Prop()
+  lastname: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  avatar: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
